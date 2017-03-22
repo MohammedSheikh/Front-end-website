b1efe64a1e2574a5024acd27aac196a0ebcf6f00
yaseen <p14159245@my365.dmu.ac.uk>
2017-03-22 22:57:39 +0000

    15 Commit
    Continued working on javascript

--- LimitlessVapes/resources/javascript/script.js
+++ LimitlessVapes/resources/javascript/script.js
@@ -70,7 +70,13 @@ $(document).ready(function() {
 
               nav.slideToggle(200);
 
-            
+              if (icon.hasClass('fa-bars')) {
+                  icon.addClass('fa-times');
+                  icon.removeClass('fa-bars');
+              } else {
+                  icon.addClass('fa-bars');
+                  icon.removeClass('fa-times');
+              }
           });
 
     });
